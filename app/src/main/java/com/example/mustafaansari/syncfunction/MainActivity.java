8a0b42d59e6d76a4ce726a67231efa47de24c694
mustafa.ansari <mustufa_ayub@hotmail.com>
2018-09-26 12:25:17 +0500

    commit

--- app/src/main/java/com/example/mustafaansari/syncfunction/MainActivity.java
+++ app/src/main/java/com/example/mustafaansari/syncfunction/MainActivity.java
@@ -1,17 +1,12 @@
 package com.example.mustafaansari.syncfunction;
 
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.example.mustafaansari.syncfunction.model.Cluster;
-import com.example.mustafaansari.syncfunction.model.User;
-import com.example.mustafaansari.syncfunction.responses.ClusterResponse;
-import com.example.mustafaansari.syncfunction.responses.UserResponse;
 import com.example.mustafaansari.syncfunction.services.RetrofitClient;
 import com.kaopiz.kprogresshud.KProgressHUD;
 
@@ -19,7 +14,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 import java.io.IOException;
-import java.util.List;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -90,43 +84,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-
-//    private void getClusters() {
-//
-//
-//        Call<ResponseBody> call2 = RetrofitClient.api.getClusters();
-//        call2.enqueue(new Callback<ResponseBody>() {
-//            @Override
-//            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-//                Toast.makeText(MainActivity.this, "Clusters synced..", Toast.LENGTH_SHORT).show();
-//                dismissProgress();
-//                getUsers();
-//                if (response.isSuccessful()) {
-//
-//                    try {
-//                        String data = response.body().string();
-//                        JSONArray jsonArray = new JSONArray(data);
-//                        for (int i = 0; i < jsonArray.length(); i++) {
-//                            Log.d(TAG, "onResponse: clusters " + jsonArray.get(i));
-//                        }
-//                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                    } catch (JSONException e) {
-//                        e.printStackTrace();
-//                    }
-//
-//
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<ResponseBody> call, Throwable t) {
-//
-//
-//            }
-//        });
-//
-//    }
 
     private void dismissProgress() {
         hud.dismiss();
--- app/src/main/java/com/example/mustafaansari/syncfunction/services/APIs.java
+++ app/src/main/java/com/example/mustafaansari/syncfunction/services/APIs.java
@@ -1,8 +1,5 @@
 package com.example.mustafaansari.syncfunction.services;
 
-import com.example.mustafaansari.syncfunction.responses.ClusterResponse;
-import com.example.mustafaansari.syncfunction.responses.UserResponse;
-
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -12,11 +9,6 @@ public interface APIs {
 
     @GET("posts")
     Call<ResponseBody> getPosts();
-
-    @GET("getclusters.php")
-    Call<ResponseBody> getClusters();
-
-
 
 
 
